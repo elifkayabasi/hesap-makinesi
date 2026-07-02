e4562f6b5ab5ab6ac516ad32e75bb68894664b7f
elifkayabasi <elifkybssi@gmail.com>
2026-07-02 11:13:38 +0300

    refactor: kod temizlendi

--- Program.cs
+++ Program.cs
@@ -1,21 +1,11 @@
-// Program.cs
-// Bu dosya, uygulamanın başlangıç noktasıdır (entry point).
-// Windows Forms uygulamaları buradan başlar.
-
 namespace HesapMakinesi;
 
 static class Program
 {
-    /// <summary>
-    /// Uygulamanın giriş noktası (Main metodu).
-    /// </summary>
-    [STAThread] // Windows Forms için gerekli thread modeli
+    [STAThread]
     static void Main()
     {
-        // Uygulama yapılandırmasını başlat (DPI ayarları vb.)
         ApplicationConfiguration.Initialize();
-
-        // HesapMakinesiForm formunu oluştur ve çalıştır
         Application.Run(new HesapMakinesiForm());
     }
 }
